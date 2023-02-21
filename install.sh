8f72a687572c1e396d2355ccfcee842c91d65ac0
sumeshi <sum3sh1@protonmail.com>
2023-02-21 23:14:37 +0900

    feat: add xwindow, i3 settings

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ mkdir -p ~/.config/fish
 
 ln -sf "`pwd`/vimrc" ~/.vimrc
 ln -sf "`pwd`/tmux.conf" ~/.tmux.conf
+ln -sf "`pwd`/.Xmodmap" ~/.Xmodmap
+ln -sf "`pwd`/.xprofile" ~/.xprofile
+ln -sf "`pwd`/i3config" ~/.config/i3/config
 ln -sf "`pwd`/config.fish" ~/.config/fish/config.fish
 ln -sf "`pwd`/.gitconfig" ~/.gitconfig
 ln -sf "`pwd`/.gitconfig_nakano" ~/.gitconfig_nakano
@@ -17,7 +20,7 @@ ln -sf "`pwd`/.gitignore_global" ~/.gitignore_global
 sudo pacman -Syyu
 
 ## install homebrew
-sudo pacman -Sy procps curl file git fish peco
+sudo pacman -Sy procps curl file git fish peco feh yay xorg-xmodmap
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## install fisher
